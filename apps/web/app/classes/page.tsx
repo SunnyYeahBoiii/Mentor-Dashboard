1878c907aab83a89a645b9a8daeeca6b06b72ac1
SunnyYeahBoiii <vuminhphuongvt12345@gmail.com>
2026-02-20 04:17:08 +0000

    feat: Redesign the class card component with detailed information and action buttons, add a page title to the class listing, and introduce a currency formatting utility.

--- apps/web/app/classes/page.tsx
+++ apps/web/app/classes/page.tsx
@@ -3,12 +3,14 @@
 import { classes as ClassesList } from "@/utils/mock-data";
 import { classInfoCreateDto } from "@/dtos/class.dto";
 import { ClassCard } from "@/components/ClassCard";
+import { lusitana } from "@/utils/fonts";
 
 export default function ClassPage() {
     const classes = ClassesList.slice(0, 1);
 
     return (
         <div className="w-4/5 m-2 ml-0">
+            <h3 className={`${lusitana.className} text-2xl my-4 mb-6`}>Danh sách lớp</h3>
             {
                 classes.map((classInfo) => (
                     <ClassCard key={classInfo.id} id={classInfo.id} name={classInfo.name} section_count={classInfo.section_count} section_fee={classInfo.section_fee} student_count={classInfo.students_count} />
--- apps/web/components/ClassCard.tsx
+++ apps/web/components/ClassCard.tsx
@@ -1,4 +1,6 @@
 
+import { VNDFormat } from "@/utils/funcs";
+
 interface ClassCardProps {
     id: string;
     name: string;
@@ -9,12 +11,37 @@ interface ClassCardProps {
 
 export function ClassCard({ id, name, section_count, section_fee, student_count }: ClassCardProps) {
     return (
-        <div className="w-full bg- flex flex-row justify-between">
-            <p className="w-[30%] text-center">{id}</p>
-            <p className="w-[30%] text-center">{name}</p>
-            <p className="w-[30%] text-center">{section_count}</p>
-            <p className="w-[30%] text-center">{section_fee}</p>
-            <p className="w-[40%] text-center">{student_count}</p>
-        </div>
+        <div className="w-full rounded-xl bg-(--dark-white) flex flex-col justify-between px-4 py-6 gap-5">
+            <span className="flex flex-row justify-between gap-5">
+                <div className="flex-8 bg-white rounded-sm px-2 py-1">
+                    <p className="text-left text-xs text-black/50">Tên lớp</p>
+                    <p className="text-left ml-2">{name}</p>
+                </div>
+                <div className="flex flex-1 rounded-sm px-2 py-1 justify-center items-center bg-white hover:bg-black/10 cursor-pointer">
+                    <p className="text-center">Chỉnh sửa</p>
+                </div>
+                <div className="flex flex-1 rounded-sm px-2 py-1 justify-center items-center bg-red-300 hover:bg-red-500 cursor-pointer">
+                    <p className="text-center">Xóa lớp</p>
+                </div>
+            </span >
+            <span className="flex flex-row justify-between gap-5">
+                <div className="flex flex-1 flex-col bg-white rounded-sm px-2 py-1">
+                    <p className="text-left text-xs text-black/50">Mã lớp</p>
+                    <p className="text-left ml-2">{id}</p>
+                </div>
+                <div className="flex flex-1 flex-col bg-white rounded-sm px-2 py-1">
+                    <p className="text-left text-xs text-black/50">Số lượng học sinh</p>
+                    <p className="text-left ml-2">{student_count} học sinh</p>
+                </div>
+                <div className="flex flex-1 flex-col bg-white rounded-sm px-2 py-1">
+                    <p className="text-left text-xs text-black/50">Phí buổi học</p>
+                    <p className="text-left ml-2">{VNDFormat(section_fee)}</p>
+                </div>
+                <div className="flex flex-1 flex-col bg-white rounded-sm px-2 py-1">
+                    <p className="text-left text-xs text-black/50">Số buổi học hiện tại</p>
+                    <p className="text-left ml-2">{section_count} buổi</p>
+                </div>
+            </span>
+        </div >
     );
 }
