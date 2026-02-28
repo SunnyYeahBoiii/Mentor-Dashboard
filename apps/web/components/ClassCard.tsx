fd58c6ba6eb280cd14b4f43657654c3035db9851
SunnyYeahBoiii <vuminhphuongvt12345@gmail.com>
2026-02-28 06:54:21 +0000

    feat: enhance layout of ClassCard, CurrentSectionCard and SectionCard

--- apps/web/components/ClassCard.tsx
+++ apps/web/components/ClassCard.tsx
@@ -14,35 +14,42 @@ export function ClassCard({ id, name, section_count, section_fee, student_count 
     return (
         <div className="w-full rounded-xl bg-(--dark-white) flex flex-col justify-between p-2 gap-2">
             <span className="flex flex-row justify-between gap-5">
-                <div className="flex-8 bg-white rounded-sm px-2 py-2">
-                    <p className="text-left text-xs text-black/50">Tên lớp</p>
-                    <p className="text-left">{name}</p>
-                </div>
-                <Link className="flex flex-1 px-2 py-2 rounded-sm justify-center items-center bg-white hover:bg-black/10 cursor-pointer" href={`/classes/${id}`}>
-                    <div className="flex">
-                        <p className="text-center">Thông tin</p>
+                <div className="flex-3 bg-white rounded-sm">
+                    <div className="m-2">
+                        <p className="text-left text-xs text-black/50">Tên lớp</p>
+                        <p className="text-left">{name}</p>
                     </div>
-                </Link>
-                <div className="flex flex-1 rounded-sm px-2 py-1 justify-center items-center bg-white hover:bg-red-300 cursor-pointer">
-                    <p className="text-center">Xóa lớp</p>
+                </div>
+                <div className="flex flex-1 flex-row gap-5">
+                    <Link className="flex flex-1 px-2 py-2 rounded-sm justify-center items-center bg-white hover:bg-black/10 cursor-pointer" href={`/classes/${id}`}>
+                        <p className="text-center">Thông tin</p>
+                    </Link>
+                    <div className="flex flex-1 rounded-sm px-2 py-2 justify-center items-center bg-white hover:bg-red-300 cursor-pointer">
+                        <p className="text-center">Xóa lớp</p>
+                    </div>
                 </div>
             </span >
             <span className="flex flex-row justify-between gap-5">
-                <div className="flex w-1/5 flex-1 flex-col bg-white rounded-sm px-2 py-2">
-                    <p className="text-left text-xs text-black/50">Mã lớp</p>
-                    <p className="text-left whitespace-nowrap overflow-scroll no-scrollbar h-5">{id}</p>
-                </div>
-                <div className="flex w-1/5 flex-1 flex-col bg-white rounded-sm px-2 py-2">
-                    <p className="text-left text-xs text-black/50">Số lượng học sinh</p>
-                    <p className="text-left">{student_count} học sinh</p>
-                </div>
-                <div className="flex w-1/5 flex-1 flex-col bg-white rounded-sm px-2 py-2">
-                    <p className="text-left text-xs text-black/50">Phí buổi học</p>
-                    <p className="text-left">{VNDFormat(section_fee)}</p>
-                </div>
-                <div className="flex w-1/5 flex-1 flex-col bg-white rounded-sm px-2 py-2">
-                    <p className="text-left text-xs text-black/50">Số buổi học hiện tại</p>
-                    <p className="text-left">{section_count} buổi</p>
+                <span className="flex flex-3 flex-row gap-5 rounded-md ">
+                    <div className="flex flex-1 flex-col bg-white rounded-sm px-2 py-2">
+                        <p className="text-left text-xs text-black/50">Mã lớp</p>
+                        <p className="text-left whitespace-nowrap overflow-scroll no-scrollbar h-5">{id}</p>
+                    </div>
+                    <div className="flex flex-1 flex-col bg-white rounded-sm px-2 py-2">
+                        <p className="text-left text-xs text-black/50">Số lượng học sinh</p>
+                        <p className="text-left">{student_count} học sinh</p>
+                    </div>
+                    <div className="flex flex-1 flex-col bg-white rounded-sm px-2 py-2">
+                        <p className="text-left text-xs text-black/50">Phí buổi học</p>
+                        <p className="text-left">{VNDFormat(section_fee)}</p>
+                    </div>
+                </span>
+
+                <div className="flex flex-1 flex-row gap-5">
+                    <div className="flex flex-1 flex-col bg-white rounded-sm px-2 py-2">
+                        <p className="text-left text-xs text-black/50">Số buổi học hiện tại</p>
+                        <p className="text-left">{section_count} buổi</p>
+                    </div>
                 </div>
             </span>
         </div >
--- apps/web/components/CurrentSectionCard.tsx
+++ apps/web/components/CurrentSectionCard.tsx
@@ -13,28 +13,34 @@ export default function CurrentSectionCard({ id, name, className, startTime, mee
     return (
         <div className="w-full rounded-xl bg-(--dark-white) flex flex-col justify-between p-2 py-2 gap-2">
             <span className="flex flex-row justify-between gap-5">
-                <div className="flex-8 bg-white rounded-sm px-2 py-1">
-                    <p className="text-left text-xs text-black/50">Lớp học</p>
-                    <p className="text-left">{className}</p>
-                </div>
-                <Link className="flex flex-1 px-2 py-1 rounded-sm justify-center items-center bg-white hover:bg-green-300 cursor-pointer" href={`/current-sessions/end-session/${id}`}>
-                    <div className="flex">
-                        <p className="text-center">Kết thúc</p>
+                <div className="flex-3 bg-white rounded-sm">
+                    <div className="m-2">
+                        <p className="text-left text-xs text-black/50">Lớp học</p>
+                        <p className="text-left">{className}</p>
                     </div>
-                </Link>
-                <div className="flex flex-1 rounded-sm px-2 py-1 justify-center items-center bg-white hover:bg-red-300 cursor-pointer">
-                    <p className="text-center">Xóa</p>
+                </div>
+                <div className="flex flex-row flex-1 gap-5">
+                    <Link className="flex flex-1 px-2 py-2 rounded-sm justify-center items-center bg-white hover:bg-green-300 cursor-pointer" href={`/current-sessions/end-session/${id}`}>
+                        <p className="text-center">Kết thúc</p>
+                    </Link>
+                    <div className="flex flex-1 rounded-sm px-2 py-2 justify-center items-center bg-white hover:bg-red-300 cursor-pointer">
+                        <p className="text-center">Xóa</p>
+                    </div>
                 </div>
             </span>
 
             <span className="flex flex-row justify-between gap-5">
-                <div className="flex-10 bg-white rounded-sm px-2 py-1">
-                    <p className="text-left text-xs text-black/50">Thời gian</p>
-                    <p className="text-left">{startTime.toLocaleDateString()} | Bắt đầu: {startTime.toLocaleTimeString()} - Đang diễn ra</p>
+                <div className="flex-3 bg-white rounded-sm">
+                    <div className="m-2">
+                        <p className="text-left text-xs text-black/50">Thời gian</p>
+                        <p className="text-left">{startTime.toLocaleDateString()} | Bắt đầu: {startTime.toLocaleTimeString()} - Đang diễn ra</p>
+                    </div>
                 </div>
-                <div className="flex-3 bg-white rounded-sm px-2 py-1">
-                    <p className="text-left text-xs text-black/50">Tên buổi học</p>
-                    <p className="text-left">{name}</p>
+                <div className="flex-1 bg-white rounded-sm">
+                    <div className="m-2">
+                        <p className="text-left text-xs text-black/50">Tên buổi học</p>
+                        <p className="text-left">{name}</p>
+                    </div>
                 </div>
             </span>
 
