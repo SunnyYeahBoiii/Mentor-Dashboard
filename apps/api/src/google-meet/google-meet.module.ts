85136fc096c003fc926556f03bac15aff7ad0046
sunny <vuminhphuongvt12345@gmail.com>
2026-05-24 19:58:48 +0700

    fix(api): Import PrismaModule in MeetModule for CookieAuthGuard resolution

--- apps/api/src/google-meet/google-meet.module.ts
+++ apps/api/src/google-meet/google-meet.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { MeetService } from './google-meet.service';
 import { MeetController } from './google-meet.controller';
 import { ConfigModule } from '@nestjs/config';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
     providers: [MeetService],
     controllers: [MeetController],
     exports: [MeetService],
-    imports: [ConfigModule],
+    imports: [ConfigModule, PrismaModule],
 })
 export class MeetModule {}
